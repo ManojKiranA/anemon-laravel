81a3aa410f66ddc13fe41c447b63b1c9ff49fd43
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2022-01-07 00:39:21 +0300

    It changes tfullscreencard to fullscreenlayout

--- resources/js/Components/Auth/TLogin.vue
+++ resources/js/Components/Auth/TLogin.vue
@@ -1,5 +1,5 @@
 <template>
-  <t-full-screen-card :bg-image="bgImage" :color="bgColor" :gradient-direction="bgGradientDirection">
+  <full-screen-layout :bg-image="bgImage" :color="bgColor" :gradient-direction="bgGradientDirection">
     <div
       :class="[
                 'relative',
@@ -132,7 +132,7 @@
         </transition>
       </div>
     </div>
-  </t-full-screen-card>
+  </full-screen-layout>
 </template>
 
 <script>
@@ -141,7 +141,7 @@ import { loginStyleMixin } from "@/Mixins/Styles/loginStyleMixin";
 import TAlert from "@/Components/Alert/TAlert";
 import TBellIcon from "@/Components/Icon/TBellIcon";
 import TButton from "@/Components/Button/TButton";
-import TFullScreenCard from "@/Components/Card/TFullScreenCard";
+import FullScreenLayout from "@/Layouts/FullScreenLayout";
 import TInputGroup from "@/Components/Form/TInputGroup";
 import TInputText from "@/Components/Form/Inputs/TInputText";
 import { Link } from "@inertiajs/inertia-vue3";
@@ -157,7 +157,7 @@ export default defineComponent({
     TAlert,
     TBellIcon,
     TButton,
-    TFullScreenCard,
+    FullScreenLayout,
     TInputGroup,
     TInputText,
     Link
